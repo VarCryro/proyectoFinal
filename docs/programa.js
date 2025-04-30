96fd17de5fb9dfacbe8da4e7805dc22519147692
Varcry <criss.vngs395@gmail.com>
2025-04-30 16:10:51 -0500

    0.6.1

--- docs/programa.js
+++ docs/programa.js
@@ -1,2 +1,13 @@
-// Aquí puedes agregar código JS si quieres, por ejemplo:
-console.log("¡Página cargada exitosamente!");
+// Inicializar el mapa centrado en tu cuadra
+var map = L.map('map').setView([4.541920221090718, -74.09174847415579], 18);
+
+// Capa de OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Añadir marcador
+L.marker([4.541920221090718, -74.09174847415579]).addTo(map)
+  .bindPopup('¡Aquí está mi cuadra!')
+  .openPopup();
