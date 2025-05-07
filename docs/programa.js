d0557dd535056c181d4cf8e92d3f27382eeacb33
Varcry <criss.vngs395@gmail.com>
2025-05-06 22:03:39 -0500

    0.6.2

--- docs/programa.js
+++ docs/programa.js
@@ -11,3 +11,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([4.541920221090718, -74.09174847415579]).addTo(map)
   .bindPopup('¡Aquí está mi cuadra!')
   .openPopup();
+
+  console.log("Hola Mundo");
+
+let x = document.getElementById("tarea");
+console.log(x.innerHTML);
+
+x.innerHTML = "Hola desde JS";
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 9 !== 0 && i % 13 !== 0) {
+    console.log(i);
+  }
+}
